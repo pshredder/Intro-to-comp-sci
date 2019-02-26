0b72e4f2498447c14bcd18a082082b38f0c4b07a
czephyr <czephyr@HS-D210-17.MOHS.local>
2019-02-26 12:12:54 -0500

    rex

--- Lesson 2 variables/src/Lesson2_15.java
+++ Lesson 2 variables/src/Lesson2_15.java
@@ -4,12 +4,12 @@ public static void main(String[] args) {
 String p= "Raxand";
 String a = "Professional Killer";
 String b = "Mercinery";
-String c = " Decimator";
+String c = "Decimator";
 int d = 11;
 String e = "Elementory  drawing";
-String f = " Way of living ";
+String f = "Way of living ";
 String g = "Death sintence";
-System.out.println(p + a + "\n" + b + c + "\n"+ d + e+ "\n"+ f + g);
+System.out.println(p + " - " + a + "\n" + b + " - " + d + "\n"+ c + " - " + e+ "\n"+ f + " - " + g);
 
 String h = "\n";
 System.out.println(h + "I \"love\" \n books" );
--- Lesson3/src/StringMinup.java
+++ Lesson3/src/StringMinup.java
@@ -3,19 +3,27 @@ public class StringMinup {
 
 	public static void main(String[] args) {
 	
-	String x = "Clefmael Zephyr";
-	int r = x.length();
-	System.out.println(r - 1);
+	String x = "The number of rabbits is";
+	int argh = 129;
+	System.out.println(x + " " + argh + ".");
 	
-	System.out.println(x);
-	System.out.println(x.substring(9) + x);
-	String L = x.substring(9);
-	System.out.println(L);
-	String F = x.substring(0,8);
-	System.out.println(F);
-	System.out.println(F + " " +L);
+	String g = "Computer Science is for nerds";
+	System.out.println(g.toLowerCase());
+	
+	String m = "\"Look here!\"";
+	int h = m.length() - 2;
+	System.out.println( m + " has" + h + " characters.");
+	
+	String q = "Hello again";
+	System.out.println( q.substring(0,5) + "\n" + q);
+	String v = "A backslash looks like this \\, ...right?";
+	System.out.println(v);
 	
 	
+	String s1 = "Allan Alda";
+	String s2 = "Jhon Wayne";
+	String s3 = "Gregory Peck";
+	System.out.println();
 	}
 
 }
